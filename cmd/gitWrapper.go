cc1ca5ae6cbd5e1b6ef302d52dffe13478fc4c01
Krakakanok <krakakanok@protonmail.com>
2019-02-13 20:08:40 +0100

    docs(gitwrapper): generated a doc for the command

--- cmd/gitWrapper.go
+++ cmd/gitWrapper.go
@@ -11,13 +11,7 @@ import (
 // gitWrapperCmd represents the gitWrapper command
 var gitWrapperCmd = &cobra.Command{
 	Use:   "gitWrapper",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Wrapper to git commit command",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("gitWrapper called")
 	},
