65dff9e910b9b8cbac275d34a8164d7487dd670f
Krakakanok <krakakanok@protonmail.com>
2019-02-13 19:37:36 +0100

    docs(cobra): generated the root cobra doc

--- cmd/root.go
+++ cmd/root.go
@@ -15,13 +15,7 @@ var cfgFile string
 
 var rootCmd = &cobra.Command{
 	Use:   "convCommitsUtils",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "A utils to use conventional commits",
 }
 
 func Execute() {
