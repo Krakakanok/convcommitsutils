3109272a7e84d12233f381e4c494c9e5ab2420ea
Krakakanok <krakakanok@protonmail.com>
2019-02-13 20:15:02 +0100

    feat(gitwrapper): implemented the wrapper

--- cmd/gitWrapper.go
+++ cmd/gitWrapper.go
@@ -4,16 +4,100 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
+	"gopkg.in/AlecAivazis/survey.v1"
 )
+
+var qs = []*survey.Question{
+	{
+		Name: "type",
+		Prompt: &survey.Select{
+			Message: "Choose a type:",
+			Options: []string{"feat", "fix", "docs", "style", "refactor", "test", "chrore"},
+			Default: "feat",
+		},
+	},
+	{
+		Name:      "scope",
+		Prompt:    &survey.Input{Message: "What is the scope?"},
+		Transform: survey.ToLower,
+	},
+	{
+		Name:     "subject",
+		Prompt:   &survey.Input{Message: "What is the subject?"},
+		Validate: survey.Required,
+	},
+	{
+		Name:      "body",
+		Prompt:    &survey.Input{Message: "What is the body?"},
+		Transform: survey.ToLower,
+	},
+	{
+		Name:      "footer",
+		Prompt:    &survey.Input{Message: "What is the footer?"},
+		Transform: survey.ToLower,
+	},
+}
 
 // gitWrapperCmd represents the gitWrapper command
 var gitWrapperCmd = &cobra.Command{
 	Use:   "gitWrapper",
 	Short: "Wrapper to git commit command",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("gitWrapper called")
+
+		message_file := ".git/COMMIT_EDITMSG"
+
+		//message := ""
+		var message [5]string
+
+		answers := struct {
+			Type    string
+			Scope   string
+			Subject string
+			Body    string
+			Footer  string
+		}{}
+
+		// perform the questions
+		err := survey.Ask(qs, &answers)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		if answers.Scope == "" {
+			message[0] = fmt.Sprintf("%s: %s\n", answers.Type, answers.Subject)
+		} else {
+			message[0] = fmt.Sprintf("%s(%s): %s\n", answers.Type, answers.Scope, answers.Subject)
+		}
+
+		if answers.Body != "" {
+			message[1] = " "
+			message[2] = fmt.Sprintf("\n%s\n", answers.Body)
+			message[3] = " "
+			message[4] = fmt.Sprintf("\n%s\n", answers.Footer)
+		}
+
+		f, err := os.OpenFile(message_file, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			return
+		}
+
+		for _, line := range message {
+			f.WriteString(line)
+			if err != nil {
+				f.Close()
+				return
+			}
+		}
+
+		err = f.Close()
+		if err != nil {
+			return
+		}
+
 	},
 }
 
